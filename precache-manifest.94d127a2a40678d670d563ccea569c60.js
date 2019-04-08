d6333bd540c7d9d59594af4e432be8ac23f7062e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-08 07:49:14 +0000

    Deploy makanlah/sambal to github.com/makanlah/sambal.git:gh-pages

---
+++ precache-manifest.94d127a2a40678d670d563ccea569c60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea15ede665cd33ef6c6",
+    "url": "/static/css/main.d5ab84d7.chunk.css"
+  },
+  {
+    "revision": "6ea15ede665cd33ef6c6",
+    "url": "/static/js/main.6ea15ede.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "97132fcfd568fee3b96b",
+    "url": "/static/js/2.97132fcf.chunk.js"
+  },
+  {
+    "revision": "61973b06766229f2054750086e06afbd",
+    "url": "/index.html"
+  }
+];
